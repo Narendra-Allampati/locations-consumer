80e82332b6908493879e2a59abf1c34ef4a784d4
Anders Clausen <anders.clausen1@maersk.com>
2021-09-08 09:58:38 +0100

    Daily check-in

---
+++ src/main/java/com/maersk/referencedata/locationsconsumer/LocationsConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.maersk.referencedata.locationsconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationsConsumerApplication.class, args);
+	}
+
+}
