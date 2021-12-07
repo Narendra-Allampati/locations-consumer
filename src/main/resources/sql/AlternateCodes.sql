f89d335ffd2962b6ec6341a7915de4d126733745
Anders Clausen <anders.clausen1@maersk.com>
2021-12-07 07:12:18 +0000

    Daily check in 07122021

---
+++ src/main/resources/sql/AlternateCodes.sql
@@ -0,0 +1,6 @@
+CREATE TABLE ALTERNATE_CODES
+(
+	ID										uuid,
+	ROW_ID									VARCHAR (100),
+    CODE_TYPE                              	VARCHAR (100),
+    CODE	                                VARCHAR (100));
---
+++ src/main/resources/sql/AlternateNames.sql
@@ -0,0 +1,7 @@
+CREATE TABLE ALTERNATE_NAMES
+(
+	ID										uuid,
+	ROW_ID									VARCHAR (100),
+    NAME                              		VARCHAR (100),
+    DESCRIPTION	                            VARCHAR (100),
+	STATUS									VARCHAR (100));
